69b99b0889c6e7c1b527910781d3da5c4f8b2203
AebadUQ <aebadulquadir123@gmail.com>
2025-10-26 21:40:05 +0500

    fizxe

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,11 @@
+// api.ts
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://arabsocials.duckdns.org', // HTTP, no port
+  baseURL: 'http://192.168.18.119:3000',
+    // baseURL: 'https://arabsocials.duckdns.org', // HTTP, no port
+
   timeout: 10000,
 });
 
@@ -14,39 +17,31 @@ export const setupInterceptors = (logout: () => void) => {
       if (token) config.headers.Authorization = `Bearer ${token}`;
       return config;
     },
-    (error) => {
-      console.log('Axios Request Error:', error);
-      return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
   );
 
   // ✅ Response interceptor
   api.interceptors.response.use(
     (response) => response,
     (error) => {
-      // Log full error object
-      console.log('Axios Response Error:', error);
-
       if (error?.status === 401) {
+        // Unauthorized → Logout user
         console.log('401 Unauthorized → logging out');
-        logout();
+        logout(); // Context me logout call
       }
 
+      // Original error handling
       if (error.response && error.response.data) {
         const data = error.response.data;
         let message = 'Something went wrong';
         if (typeof data.message === 'string') message = data.message;
         else if (Array.isArray(data.message)) message = data.message.join(', ');
         else if (typeof data.error === 'string') message = data.error;
-
-        console.log('API Error Message:', message);
         return Promise.reject(new Error(message));
       } else if (error.message) {
-        console.log('API Error Message:', error.message);
         return Promise.reject(new Error(error.message));
       }
 
-      console.log('API Unknown Error:', error);
       return Promise.reject(new Error('An unknown error occurred'));
     }
   );
