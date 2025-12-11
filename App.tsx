4ed6c1734a540e95c91872ff6cde5654dcb371f9
AebadUQ <aebadulquadir123@gmail.com>
2025-12-12 02:48:49 +0500

    configurations

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
@@ -7,10 +7,43 @@ import { AuthProvider } from "@/context/Authcontext";
 import { PaperProvider } from "react-native-paper";
 import CustomSnackbar from "@/components/common/CustomSnackbar";
 import AppContent from "@/context/AppContent";
+  import {Alert, PermissionsAndroid} from 'react-native';
+  import messaging from '@react-native-firebase/messaging';
 
+
+  
 const queryClient = new QueryClient();
 
 export default function App() {
+  useEffect(()=>{
+  requestPermissionAndroid()
+},[])
+
+const requestPermissionAndroid = async()=>{
+  const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+  if(granted === PermissionsAndroid.RESULTS.GRANTED){
+    // Alert.alert('Permission Granted')
+    getToken()
+  }
+  else{
+        // Alert.alert('Permission Denied')
+
+  }
+
+}
+useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+  const getToken = async()=>{
+    console.log("sss")
+    const token= await messaging().getToken()
+      console.log("token",token);
+
+  }
   return (
     <PaperProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
